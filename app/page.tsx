c366dc1bceedd6a4971d91cf1697e7a9c94e0d30
hieusita11104 <Duchieu111004@gmail.com>
2025-02-11 20:56:38 +0700

    Initial commit with Next.js app

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+function MyComponent() {
+  return (
+    <div>
+      <h1>ReactJS with JSX</h1>
+      <p>Welcome to the world of ReactJS!</p>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </div>
+  );
+}
+
+
+export default MyComponent;
+
